f18d5865951f37f111b65ed0ec72291a2a431705
chiuweilun1107 <chiuweilun1107@gmail.com>
2025-06-14 15:36:43 +0800

    feat: 增加 LearningPathSection 區塊的上下邊緣空間 - 上邊距從 py-20 增加到 py-32 - 下邊距從 pb-60 增加到 pb-80 - 讓整個課程區塊有更多呼吸空間

--- src/components/course/LearningPathSection.tsx
+++ src/components/course/LearningPathSection.tsx
@@ -195,7 +195,7 @@ const LearningPathSection: React.FC = () => {
   }
 
   return (
-    <section className="py-20 pb-60 bg-gradient-to-br from-gray-50 to-white">
+    <section className="py-32 pb-80 bg-gradient-to-br from-gray-50 to-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="flex flex-col md:flex-row md:justify-between md:items-center mb-12">
